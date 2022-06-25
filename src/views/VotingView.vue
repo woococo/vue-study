04d6a6712f79e3fdda5f28348bdb8eae1765bc80
zyeon <101681351+woococo@users.noreply.github.com>
2022-06-25 13:39:15 +0900

    VotiongView

---
+++ src/views/VotingView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="voting">
+    <h1>This is an Voiting page</h1>
+    <!--/* eslint-disable */-->
+    <span class="msg">안녕하세요~!</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.voting {
+  .msg {
+    font-size: "13px";
+    color: red;
+  }
+}
+</style>
