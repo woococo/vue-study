622517741594d34e23970a00f0447136df5b6211
zyeon <101681351+woococo@users.noreply.github.com>
2022-06-25 13:38:41 +0900

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ResultView from "../views/ResultView.vue";
+import DetailView from "../views/DetailView.vue";
+
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/voting",
+    name: "voting",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/VotingView.vue"),
+  },
+  {
+    path: "/result",
+    name: "result",
+    component: ResultView,
+  },
+  {
+    path: "/result/detail",
+    name: "detail",
+    component: DetailView,
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
