efcdef3c119f8a43dea2294304d284fa3392e76c
zyeon <101681351+woococo@users.noreply.github.com>
2022-06-25 13:38:58 +0900

    HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-for="alias in items" :key="alias.name">
+      <span>{{ alias.name }}</span> : <span>{{ alias.age }}</span><br>
+    </div>
+    <button @click="btnClick">click me!</button>
+    <HelloWorld msg="Welcome to Your Vue.js App" name="남지현" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// @ is an alias to /src 
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     HelloWorld
+  },
+  created() {
+    console.log("in created.");
+  },
+  mounted() {
+    console.log("in mounted!");
+  },
+  beforeUpdate() {
+    console.log('in before updated!');
+  },
+  data () {
+    return {
+      items: [
+        {name: "john", age: 31},
+        {name: "tom", age: 12},
+        {name: "danny", age: 22},
+        {name: "lydia", age: 30},
+        {name: "elena", age: 28},
+        {name: "judy", age: 22}
+      ]
+    }
+  },  
+  methods: {
+    btnClick() {
+      alert("button clicked!");
+    }
   }
-}
+};
 </script>
